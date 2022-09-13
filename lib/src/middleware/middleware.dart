86b3bc8f6f9e727948fb9cab87d7e7e1a51ce180
FMotalleb <fmotalleb@gmail.com>
2022-09-13 09:36:14 +0430

    [Feat,Doc] added redirect to middleware

--- lib/src/middleware/middleware.dart
+++ lib/src/middleware/middleware.dart
@@ -18,9 +18,21 @@ abstract class GoMiddleware<T extends Object> {
 
   /// [validate] is simple validation method to check the parameters given to
   /// the middleware are correct so **DO NOT** use any huge computations here.
+  ///
+  /// result of this method can be overriden by the creation of the
+  /// [GoRouteInfused]
+  ///
+  /// this method is called before the [generateProvider] if validation was not
+  /// ignored in GoRouteInfused construction.
   @protected
   MiddlewareValidationResult? validate(
     BuildContext context,
     GoRouterState state,
   );
+
+  /// redirect to another route if returned a non-null value
+  ///
+  /// for example this can be used to handle user authentications and
+  /// permissions
+  String? redirect(GoRouterState state) => null;
 }
